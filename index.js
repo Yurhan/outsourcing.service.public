1f61620c114e7961a09fb8fcb0520070d79945d8
Yurii Andrusyshyn <yurshmir@gmail.com>
2018-06-22 19:28:55 +0300

    fixed index

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var pm2 = require('pm2');
 var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
 var maxMemory = process.env.WEB_MEMORY || 512; // " " "
 
+console.log('PM2 Before connect');
 pm2.connect(function () {
+
+  console.log('PM2 connected');
   pm2.start('./processes.json', function (err) {
     if (err) return console.error('Error while launching applications', err.stack || err);
     console.log('PM2 and application has been succesfully started');
